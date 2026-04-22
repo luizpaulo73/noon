9accd82f690bbeb039888295fa43a6ef30faeea6
luizpaulo73 <luiz.paulo2005@hotmail.com>
2026-04-22 01:06:26 -0300

    feat: Extend getGitData types to include optional since and until properties

--- src/types/gitManagement.ts
+++ src/types/gitManagement.ts
@@ -1,5 +1,13 @@
 export type getGitDataReturnType = {
     range: string;
+    since?: string;
+    until?: string;
     log: string;
     diff: string;
+}
+
+export type getGitDataInput = {
+    range?: string;
+    since?: string;
+    until?: string;
 }
